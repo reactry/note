323a70256a211058ea9cfcb1e9c24f193ca645ce
Ankur Seth <iankurseth@gmail.com>
2022-01-12 18:16:01 +0530

    Initial commit with CRA, tailwind setup and GA

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App () {
+	return (
+		<div className="App">
+			<header className="App-header bg-fuchsia-400">
+				<h2 className="text-2xl p-8 text-center">Hallo Welt</h2>
+			</header>
+		</div>
+	);
+}
+
+export default App;
