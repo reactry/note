97b8c54df574a95641112092f2156f6585ecef60
Ankur Seth <iankurseth@gmail.com>
2022-02-16 02:36:14 +0530

    Added TopTabBar component

--- src/NoteApp/NoteApp.js
+++ src/NoteApp/NoteApp.js
@@ -1,14 +1,22 @@
 import React from 'react';
 
+import TopTabBar from './TopTabBar';
 import Note from './Note';
 import Notebook from './Notebook';
 import Footer from './Footer';
+
+const tabs = [
+	{"title": "Notes"},
+	{"title": "Notebooks"}
+];
 
 
 
 export default function NoteApp () {
 	const [notes, setNotes] = React.useState([]);
 	const [notebooks, setNotebooks] = React.useState([]);
+
+	const [currentTabIndex, setCurrentTabIndex] = React.useState(0);
 
 	let noteItems = notes.map((note, i) => {
 		return <Note key={i} note={note} />;
@@ -18,8 +26,14 @@ export default function NoteApp () {
 		return <Notebook key={i} notebook={notebook} />;
 	});
 
+	const topTabBarProps = {
+		tabs, currentTabIndex, setCurrentTabIndex
+	};
+
 	return (
-		<div className="NoteApp bg-blue-200">
+		<div className="NoteApp bg-slate-100">
+
+			<TopTabBar {...topTabBarProps} />
 
 			<main className="min-h-screen max-w-4xl mx-auto px-4 py-4">
 
---
+++ src/NoteApp/TopTabBar.js
@@ -0,0 +1,23 @@
+
+
+
+export default function TopTabBar ({
+	tabs, currentTabIndex, setCurrentTabIndex
+}) {
+
+	let tabItems = tabs.map((v, i) => {
+		let tabClass = "grow px-2 py-5 duration-300";
+		tabClass += (i === currentTabIndex) ? " bg-slate-100" : " hover:bg-slate-200 cursor-pointer";
+		return (
+			<div key={i} className={tabClass} onClick={() => setCurrentTabIndex(i)}>{v.title}</div>
+		);
+	});
+
+	return (
+		<div className="TopTabBar bg-slate-300">
+			<div className="flex text-center max-w-4xl mx-auto text-sm font-bold">
+				{tabItems}
+			</div>
+		</div>
+	);
+}
